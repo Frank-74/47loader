fce825b53b00940ddfb23bc0a44df7c230437404
stephenw32768 <stephenw32768@gmail.com>
2013-08-25 12:39:23 +0000

    simple example

---
+++ examples/simple-demo/simple-demo.asm
@@ -0,0 +1,46 @@
+        org     0
+
+        .dest:  equ     0xfb00
+
+.msg_start:equ $
+        ;dm      '** 47loader **',13,13,13
+.msg_len:equ $-.msg_start
+        ld      de,.dest
+        push    bc
+        pop     hl              ; entry address now in HL
+        ld      bc,.reloc_start-.msg_len
+        add     hl,bc
+        ld      bc,.reloc_len
+        ldir
+
+        ;; load directly into screen
+        ld      ix,0x4000
+        ld      de,0x1b00
+
+        ;; GO!
+        call    loader_entry
+
+        ;; if it returned carry set, all went well
+        jr      nc,.err
+
+        ;; black border to finish
+        ;; (loader leaves zero in accumulator)
+        out     (0xfe),a
+        ret
+
+.err:
+        ;; if it returned zero set, BREAK was pressed
+        jp      z,0x1b7b        ; ROM routine indicating BREAK (code L)
+        ;; otherwise, there was a tape error
+        rst     8               ; error restart
+        defb    26              ; "R Tape loading error"
+
+.reloc_start:   equ     $
+        .phase  .dest
+
+;LOADER_THEME_ORIGINAL:equ 1
+LOADER_THEME_RAINBOW:equ 1
+;LOADER_THEME_RAINBOW_RIPPLE:equ 1
+;LOADER_THEME_LDBYTES:equ 1
+        include "47loader.asm"
+.reloc_len: equ $ - .dest
---
+++ examples/simple-demo/simple-demo.bas
@@ -0,0 +1,3 @@
+1 clear 32767
+2 randomize usr(PEEK 23635 + 256*PEEK 23636+5)
+3 pause 0
